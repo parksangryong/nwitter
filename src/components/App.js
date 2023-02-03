8658e0c00c1ed5377a08f0b8cf1a9be0d445e552
parksangryong <ppsr3478@naver.com>
2023-02-03 13:24:39 +0900

    2-2

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import AppRouter from "components/Router";
+import { useState } from "react";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <>
+    <AppRouter isLoggedIn={isLoggedIn} />
+    <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Router.js
@@ -0,0 +1,19 @@
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+
+const AppRouter = ({isLoggedIn}) => {
+    return (
+        <Router>
+            <Routes>
+                {isLoggedIn ? (
+                    <Route exact={true} path="/" element={<Home />} />
+                ) : (
+                    <Route exact={true} path="/" element={<Auth />} />
+                )}
+            </Routes>
+        </Router>
+    );
+}
+
+export default AppRouter;
