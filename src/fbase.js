b524a611d3a67ba74cf3d2fa49a43fe0206c5faf
parksangryong <ppsr3478@naver.com>
2023-02-03 15:10:38 +0900

    2/2 2

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,4 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export default firebaseConfig;
+export const authService = getAuth(app);
